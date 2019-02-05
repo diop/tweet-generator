2d01bf765e61a8cd3750d2c632e95b83276e07ef
diop <fodesdiop@gmail.com>
2019-02-04 17:31:36 -0800

    add dictionary words

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,10 +1,21 @@
-import sys 
+import sys
+import random 
 
-def process_words():
-    with open('wolof_words.txt', 'r') as f:
-        words_document = f.read().split()
-    
-    # new_word_list = words_document.split()
-    print(words_document)
-    
-process_words()
+def get_dictionary_words():
+    with open('twenty_thousand_words.txt', 'r') as f:
+        dictionary_words = f.read()
+
+    return dictionary_words.splitlines()
+
+def create_random_sentence(dictionary, num_words):
+    for _ in range(num_words):
+        return ' '.join(random.choice(dictionary))
+
+def main():
+    # https://github.com/DacioRomero/Metrogen/blob/b00c375a1f56d939253f44edfd694e19dfd4df29/Code/dictionary_words.py
+    num_words = int(sys.argv[1])
+    dictionary = get_dictionary_words()
+    print(create_random_sentence(dictionary, num_words))
+
+if '__name__' == '__main__':
+    main()
