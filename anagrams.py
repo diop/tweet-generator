a798ef5a65992887a1a7d06068a52e4d75f64940
diop <fodesdiop@gmail.com>
2019-02-06 15:12:27 -0800

    cleanup

--- anagrams.py
+++ anagrams.py
@@ -8,6 +8,9 @@ print(anagram_easy('Clint Eastwood', 'old west action'))
 
 
 def anagram_medium(str1, str2):
+    str1 = str1.replace(' ', '').lower()
+    str2 = str2.replace(' ', '').lower()
+
     if len (str1) != len(str2):
         return False
 
@@ -17,7 +20,7 @@ def anagram_medium(str1, str2):
         if letter in count:
             count[letter] += 1
         else:
-            ccount[letter] = 1
+            count[letter] = 1
 
     for letter in str2:
         if letter in count:
